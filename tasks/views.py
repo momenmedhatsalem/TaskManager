85f55cdfc4c9d76e6106d3c103d144ec1a02650a
momenmedhatsalem <momenmedhatsalem@gmail.com>
2025-01-29 15:58:22 +0200

    Added views for viewing and create tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# tasks/views.py
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    # Retrieve all tasks, but filter by the authenticated user
+    queryset = Task.objects.all()
+    # Use the TaskSerializer to format data
+    serializer_class = TaskSerializer
+    # Restrict access to authenticated users
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically associate the task with the authenticated user
+        serializer.save(user=self.request.user)
