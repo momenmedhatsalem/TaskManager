ebaca5d458f7a1a8ac65d557cdb12e89d6b89fe1
momenmedhatsalem <momenmedhatsalem@gmail.com>
2025-01-29 16:02:57 +0200

    Updated settings.py of the project to include the restframework and the tasks app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Third Party
+    'rest_framework',
+
+
+    # Apps
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +128,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Configure REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
