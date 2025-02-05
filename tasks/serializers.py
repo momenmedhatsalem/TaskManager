63a614438c2fff87e46842c4f0f6234ca4a87377
momenmedhatsalem <momenmedhatsalem@gmail.com>
2025-02-05 07:10:36 +0200

    Updated serializer.py to show only necessary fields

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,9 +1,9 @@
-# tasks/serializers.py
 from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        # Fields to be serialized (converted to JSON)
-        fields = ['id', 'title', 'description', 'completed', 'created_at', 'updated_at', 'user']
+        fields = ['id', 'title', 'description', 'completed']
+
+
